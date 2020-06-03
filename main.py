d0d6f0a46b8c3de3bd23922b7909075f52de212d
ZYFZYF <973446154@qq.com>
2020-06-03 23:46:35 +0800

    自动选取最合适的采样概率

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 from collections import defaultdict
 from scipy.stats import gaussian_kde
-from table import Table
+from table import *
 import matplotlib.pyplot as plt
 import random
 
@@ -26,7 +26,63 @@ def record_time(message):
     start_time = time.time()
 
 
+father = {}
+
+
 def init():
+    # 先把所有join的信息读出来，把column聚类
+    all_joins = []
+    for file in ['easy', 'middle', 'hard']:
+        for query in sqlparse.split(open(f'input/{file}.sql').read())[:-1]:
+            alias_to_table, _, joins = parse(query)
+            joins = [((alias_to_table[join[0][0]], join[0][1]), (alias_to_table[join[1][0]], join[1][1])) for join in
+                     joins]
+            all_joins.extend(joins)
+
+    attrs = list(set([join[0] for join in all_joins] + [join[1] for join in all_joins]))
+    # 从joins关系找出等价类
+    global father
+    father = {attr: attr for attr in attrs}
+
+    def get_father(x):
+        if father[x] == x:
+            return x
+        else:
+            y = get_father(father[x])
+            father[x] = y
+            return y
+
+    def union(x, y):
+        father[get_father(x)] = get_father(y)
+
+    for join in all_joins:
+        union(join[0], join[1])
+
+    for attr in sorted(attrs):
+        father[attr] = get_father(attr)
+        # print(attr, father[attr])
+
+    aggregate_clusters = defaultdict(list)
+    for attr in attrs:
+        aggregate_clusters[get_father(attr)].append(attr)
+
+    for k, v in aggregate_clusters.items():
+        print(k, v)
+    join_attrs = defaultdict(list)
+    for attr in attrs:
+        join_attrs[attr[0]].append(attr[1])
+
+    for k, v in join_attrs.items():
+        print(k, v)
+
+    # 为每个等价类制定一个hash函数
+    H = {}
+    for k in aggregate_clusters.keys():
+        MOD = 1000000007
+        H[k] = (random.randint(1, MOD), random.randint(1, MOD), MOD)
+
+    record_time('Aggregate join columns')
+
     global tables
     for create_sql in sqlparse.parse(open(f'data/schematext.sql').read())[:-1]:
         column_list = []
@@ -47,47 +103,9 @@ def init():
                         column_list.append((column_name, int))
                     if tk.value == 'character':
                         column_list.append((column_name, str))
-        tables[table_name] = Table(table_name, column_list)
-        # record_time(f'init table {table_name}')
-
-
-def aggregate_column():
-    all_joins = []
-    for file in ['easy', 'middle', 'hard']:
-        for query in sqlparse.split(open(f'input/{file}.sql').read())[:-1]:
-            alias_to_table, _, joins = parse(query)
-            joins = [((alias_to_table[join[0][0]], join[0][1]), (alias_to_table[join[1][0]], join[1][1])) for join in
-                     joins]
-            all_joins.extend(joins)
-
-    attrs = list(set([join[0] for join in all_joins] + [join[1] for join in all_joins]))
-    # 从joins关系找出等价类
-    father = {attr: attr for attr in attrs}
-
-    def get_father(x):
-        if father[x] == x:
-            return x
-        else:
-            y = get_father(father[x])
-            father[x] = y
-            return y
-
-    def union(x, y):
-        father[get_father(x)] = get_father(y)
-
-    for join in all_joins:
-        union(join[0], join[1])
-
-    aggregate_clusters = defaultdict(list)
-    for attr in attrs:
-        aggregate_clusters[get_father(attr)].append(attr)
-    for k, v in aggregate_clusters.items():
-        print(k, v)
-    # 为每个等价类制定一个hash函数
-    H = {}
-    for k in aggregate_clusters.keys():
-        MOD = 1000000007
-        H[k] = (random.randint(1, MOD), random.randint(1, MOD), MOD)
+        tables[table_name] = Table(table_name, column_list, join_attrs, H, father)
+        record_time(f'Init table {table_name}')
+    record_time('Finish sample tables')
 
 
 def parse(query_sql):
@@ -182,114 +200,65 @@ def parse(query_sql):
     return alias_to_table, selections, joins
 
 
+index = 0
+
+
 def solve(sql):
+    global index
+    index += 1
     alias_to_table, selections, joins = parse(sql)
-
-    join_attrs = []
-    for join in joins:
-        father[join[0]] = join[0]
-        father[join[1]] = join[1]
-        join_attrs.extend([join[0], join[1]])
-    for join in joins:
-        union(join[0], join[1])
-
-    equal_class = defaultdict(list)
-    join_attrs = list(set(join_attrs))
-    for attr in join_attrs:
-        equal_class[get_father(attr)].append(attr)
-    for ind, v in enumerate(equal_class.values()):
-        print(ind, v)
-
-    # 计算U数组，每个表被在join attr里的个数 还有每个表被join的参数
-    U = defaultdict(int)
-    u = defaultdict(list)
-    for attr in join_attrs:
-        U[attr[0]] += 1
-        u[attr[0]].append(attr[1])
-    # for k, v in U.items():
-    #     print(k, v)
-
     join_tables = list(alias_to_table.keys())
-    # 确定每个表的采样概率P
-    P = {}
-    for alias in join_tables:
-        P[alias] = min(1.0, 1.0 * 1000 / tables[alias_to_table[alias]].row_number)
-
-    # 先确定一个表采样10000条
-    # first_table = join_tables[0]
-    # P[first_table] = 1.0 * 10000 / tables[alias_to_table[first_table]].row_number
-    # 通过边逐渐求出其他表的采样概率(不太靠谱，有的直接变成0了
-    # update_exist = True
-    # while update_exist:
-    #     update_exist = False
-    #     for join in joins:
-    #         x = join[0][0]
-    #         y = join[1][0]
-    #         if P[x] == 0:
-    #             x, y = y, x
-    #         if P[x] != 0 and P[y] == 0:
-    #             update_exist = True
-    #             P[y] = P[x] ** (U[y] / U[x])
-    for alias in join_tables:
-        print(alias, U[alias], P[alias], tables[alias_to_table[alias]].row_number,
-              round(tables[alias_to_table[alias]].row_number * P[alias]))
-
-    # 采样得到Samples
-    S = defaultdict(list)
-    for alias in join_tables:
-        table = alias_to_table[alias]
-        values = pd.read_csv(f'data/{table}.csv', names=tables[table].columns).values
-        for value in values:
-            flag = True
-            # 要保证每个attr都<一定的概率才被采样
-            for attr in u[alias]:
-                if attr != 'company_type_id':
-                    a, b, p = H[get_father((alias, attr))]
-                    ind = tables[table].column_ind[attr]
-                    if value[ind]:
-                        if type(value[ind]) == int:
-                            hv = 1.0 * (a * value[ind] + b) % p / p
-                        else:
-                            hv = 1.0 * (a * hash(value[ind] + b) % p) / p
-                    else:
-                        hv = 2
-                    if hv >= P[alias] ** (1.0 / U[alias]):
-                        flag = False
-                        break
-            # if alias == 'mc':
-            #    print(flag, hv, P[alias], U[alias], P[alias] ** (1.0 / U[alias]), len(values), attr)
-            if flag:
-                S[alias].append(value)
-        record_time(f'Sample {alias} finished, sample {len(S[alias])} rows')
-
+    join_items = list(set([join[0] for join in joins] + [join[1] for join in joins]))
     # 得到采样之后join的结果大小 J
     global J
     J = 0
     # 如果是单表select的话，直接采样即可
     if len(joins) == 0:
-        alias = alias_to_table.keys()[0]
+        alias_sample_size = {alias: SAMPLE_SIZE[0] for alias in join_tables}
+        alias = list(alias_to_table.keys())[0]
         table = alias_to_table[alias]
-        for value in S[alias]:
+        sample_size = SAMPLE_SIZE[0]
+        for value in tables[table].sample_values[sample_size]:
             if tables[table].satisfy(value, selections[alias]):
                 J += 1
     # 如果是双表join的话，直接用桶即可
-    elif len(joins) == 2:
+    elif len(joins) == 1:
+        alias_sample_size = {alias: SAMPLE_SIZE[0] for alias in join_tables}
+        sample_size = SAMPLE_SIZE[0]
         Count = {alias: defaultdict(int) for alias in join_tables}
-        for alias in join_tables:
+        for item in join_items:
+            alias, attr = item
             table = alias_to_table[alias]
-            for value in S[alias]:
+            for value in tables[table].sample_values[sample_size]:
                 if tables[table].satisfy(value, selections[alias]):
                     Count[alias][value[tables[table].column_ind[attr]]] += 1
-        for v in Count[join_tables[0]].values():
-            J += Count[join_tables[0]][v] * Count[join_tables[1]][v]
+        for v in list(Count[join_items[0][0]].keys()):
+            J += Count[join_items[0][0]][v] * Count[join_items[1][0]][v]
     else:
-        return 1
         compare_columns = defaultdict(list)
         for join in joins:
             compare_columns[join[0]].append(join[1])
             compare_columns[join[1]].append(join[0])
 
         row = {}
+        join_tables = sorted(join_tables, key=lambda x: len(selections[x]), reverse=True)
+        S = {alias: defaultdict(list) for alias in join_tables}
+        alias_sample_size = {alias: SAMPLE_SIZE[-1] for alias in join_tables}
+
+        for sample_size in reversed(SAMPLE_SIZE):
+            for alias in join_tables:
+                table = alias_to_table[alias]
+                for value in tables[table].sample_values[sample_size]:
+                    if tables[table].satisfy(value, selections[alias]):
+                        S[alias][sample_size].append(value)
+                if len(S[alias][sample_size]) < 10000 ** (1.0 / len(join_tables)):
+                    alias_sample_size[alias] = sample_size
+
+        estimate_cost = 1
+        for alias in join_tables:
+            estimate_cost *= len(S[alias][alias_sample_size[alias]])
+        print(
+            f'Estimate row list is {[len(tables[alias_to_table[alias]].sample_values[alias_sample_size[alias]]) for alias in join_tables]} and select result is {[len(S[alias][alias_sample_size[alias]]) for alias in join_tables]} and total cost is {estimate_cost}')
 
         def dfs(ind):
             if ind == len(join_tables):
@@ -298,44 +267,59 @@ def solve(sql):
                 return
             alias = join_tables[ind]
             table = alias_to_table[alias]
-            for value in S[alias]:
-                if tables[table].satisfy(value, selections[alias]):
-                    flag = True
-                    for attr in u[alias]:
-                        for compare_column in compare_columns[(alias, attr)]:
-                            if compare_column in row.keys():
-                                if value[tables[table].column_ind[attr]] != row[compare_column]:
-                                    flag = False
-                                    break
-                        if not flag:
-                            break
-                    if flag:
-                        for attr in u[alias]:
-                            row[(alias, attr)] = value[tables[table].column_ind[attr]]
-                        dfs(ind + 1)
-            for attr in u[alias]:
+            for value in S[alias][alias_sample_size[alias]]:
+                flag = True
+                for attr in tables[table].u:
+                    for compare_column in compare_columns[(alias, attr)]:
+                        if compare_column in row.keys():
+                            if value[tables[table].column_ind[attr]] != row[compare_column]:
+                                flag = False
+                                break
+                    if not flag:
+                        break
+                if flag:
+                    for attr in tables[table].u:
+                        row[(alias, attr)] = value[tables[table].column_ind[attr]]
+                    dfs(ind + 1)
+            for attr in tables[table].u:
                 if (alias, attr) in row.keys():
                     row.pop((alias, attr))
 
         dfs(0)
-    print(f'Sample join answer is {J}')
+    print(f'{index}: Sample join answer is {J}, having {len(joins)} joins')
     # 计算得到Pinc
+
     Pinc = 1
-    for eq_cls in equal_class:
-        mi = 1
-        for attr in join_attrs:
-            if get_father(attr) == eq_cls:
-                mi = min(mi, P[alias] ** (1.0 / U[alias]))
-        Pinc *= mi
+    if len(joins) > 0:
+        equal_class = list(set([father[(alias_to_table[item[0]], item[1])] for item in join_items]))
+        for eq_cls in equal_class:
+            mi = 1
+            for attr in join_items:
+                table = alias_to_table[attr[0]]
+                if father[(table, attr[1])] == eq_cls:
+                    print(attr[0], attr[1], table, tables[table].U)
+                    mi = min(mi, tables[table].P[alias_sample_size[attr[0]]] ** (1.0 / tables[table].U))
+            Pinc *= mi
+    else:
+        alias = list(alias_to_table.keys())[0]
+        table = alias_to_table[alias]
+        Pinc = tables[table].P[SAMPLE_SIZE[0]]
+    if J == 0:
+        J = random.random()
     return round(J / Pinc)
+
+
+def evaluate():
+    for sql_file in ['middle', 'hard']:
+        df = pd.read_csv(f'output/{sql_file}.csv')
+        print(df.describe())
 
 
 if __name__ == '__main__':
     init()
-    record_time('Init all things')
-    aggregate_column()
     data = {}
-    for sql_file in []:
+    for sql_file in ['hard', 'middle', 'hard']:
+        index = 0
         raw = open(f'input/{sql_file}.sql').read()
         sql_stats = sqlparse.split(raw)
         ground_true = list(map(int, open(f'answer/{sql_file}.normal').readlines()))
--- table.py
+++ table.py
@@ -1,31 +1,70 @@
 import pandas as pd
 import json
+from collections import defaultdict
 
-SINGLE_SAMPLE_SIZE = 10000
+SAMPLE_SIZE = ['10000', '5000', '2500', '1000', '500', '200', '100']
 RE_SAMPLE = False
 
 
 class Table:
-    columns = []
-    int_columns = []
-    column_ind = []
-    sample_values = []
-    row_number = 0
 
-    def __init__(self, table_name, column_list):
+    def __init__(self, table_name, column_list, join_attrs, H, father):
+        self.sample_values = []
+        self.U = 0
+        self.u = []
         self.columns = [column[0] for column in column_list]
         self.column_ind = {column: ind for ind, column in enumerate(self.columns)}
         self.int_columns = [column[0] for column in column_list if column[1] == int]
+        for attr in join_attrs[table_name]:
+            if father[(table_name, attr)] not in [('movie_companies', 'company_type_id'),
+                                                  ('person_info', 'info_type_id'),
+                                                  ('movie_link', 'link_type_id'),
+                                                  ('title', 'kind_id'),
+                                                  ('cast_info', 'role_id'),
+                                                  ('complete_cast', 'status_id')]:
+                self.U += 1
+                self.u.append(attr)
+        self.U = max(self.U, 1)
+        self.P = {}
+        self.sample_values = {}
         if RE_SAMPLE:
             df = pd.read_csv(f'data/{table_name}.csv', names=self.columns)
-            sample_values = df.sample(n=min(len(df), SINGLE_SAMPLE_SIZE)).values.tolist()
-            data = {'sample_values': sample_values, 'row_number': len(df)}
+            values = df.values.tolist()
+            self.row_number = len(df)
+            for sample_size in SAMPLE_SIZE:
+                sample_rate = min(1.0, 1.0 * int(sample_size) / self.row_number)
+                if table_name == 'cast_info':
+                    sample_rate /= 10
+                self.P[sample_size] = sample_rate
+                self.sample_values[sample_size] = []
+                for value in values:
+                    flag = True
+                    # 要保证每个attr都<一定的概率才被采样
+                    for attr in self.u:
+                        a, b, p = H[father[table_name, attr]]
+                        ind = self.column_ind[attr]
+                        if value[ind]:
+                            if type(value[ind]) == int:
+                                hv = 1.0 * (a * value[ind] + b) % p / p
+                            else:
+                                hv = 1.0 * (a * hash(value[ind] + b) % p) / p
+                        else:
+                            hv = 2
+                        if hv >= self.P[sample_size] ** (1.0 / self.U):
+                            flag = False
+                            break
+                    if flag:
+                        self.sample_values[sample_size].append(value)
+            data = {'row_number': len(df), 'P': self.P, 'sample_values': self.sample_values}
             with open(f'sample/{table_name}.json', 'w') as f:
                 json.dump(data, f)
         with open(f'sample/{table_name}.json', 'r') as f:
             data = json.load(f)
             self.sample_values = data['sample_values']
             self.row_number = data['row_number']
+            self.P = data['P']
+            print(f'Sample {[len(values) for values in self.sample_values.values()]} from table {table_name}')
+            print(f"Table {table_name}'U: {self.U}, u: {self.u}")
 
     def satisfy(self, row, selections):
         for column in selections.keys():
@@ -53,4 +92,4 @@ class Table:
         count = 0
         for row in self.sample_values:
             count += self.satisfy(row, selections)
-        return 1.0 * count / SINGLE_SAMPLE_SIZE
+        return 1.0 * count / SAMPLE_SIZE
